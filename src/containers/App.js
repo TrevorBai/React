2227107766598319308eb66ff1d7956d7218eccb
Trevor Bai <baijin2011@gmail.com>
2019-11-18 10:01:36 -0500

    Refactor the App

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-
-import './App.css'
-import Person from './Person/Person'
+import classes from './App.module.css'
+import Persons from '../components/Persons/Persons'
+import Cockpit from '../components/Cockpit/Cockpit'
 
 class App extends Component {
   state = {
@@ -43,37 +43,19 @@ class App extends Component {
     let persons = null
 
     if (this.state.showPersons) {
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <Person
-              click={() => this.deletePersonHandler(index)} 
-              name={person.name}
-              age={person.age}
-              key={person.id}
-              changed={event => this.nameChangedHandler(event, person.id)} />
-          })}
-        </div>
-      )
-    }
-
-    const classes = []
-    if (this.state.persons.length <= 2) {
-      classes.push('red')
-    }
-
-    if (this.state.persons.length <= 1) {
-      classes.push('bold')
+      persons = <Persons
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangedHandler} />
     }
 
     return (
-      <div className="App">
-        <h1>Hello, how is going?</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <StyledButton
-          alt={this.state.showPersons.toString()}
-          onClick={this.togglePersonsHandler}>Toggle Persons
-        </StyledButton>
+      <div className={classes.App}>
+        <Cockpit
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          clicked={this.togglePersonsHandler}
+        />
         {persons}
       </div>
     )
